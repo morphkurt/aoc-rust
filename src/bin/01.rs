74726157de33a67ca24807f1c86f319e0eba3b54
damitha <damitha.gunawardena+github@gmail.com>
2024-12-02 10:15:07 +1100

    day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,15 +1,16 @@
 use std::collections::HashMap;
 
+
 advent_of_code::solution!(1);
 
 pub fn part_one(_input: &str) -> Option<u32> {
-    let (mut left, mut right) = parse(_input);
+    let (mut left, mut right)= parse(_input);
 
     left.sort();
     right.sort();
     let mut sum: i32 = 0;
     for i in 0..left.len() {
-        sum += (left[i] - right[i]).abs()
+          sum+=(left[i]-right[i]).abs()
     }
     Some(sum.try_into().unwrap())
 }
@@ -18,33 +19,23 @@ pub fn part_two(_input: &str) -> Option<u32> {
     let (left, right) = parse(_input);
     let mut map = HashMap::new();
     for ele in right {
-        let count: &mut i32 = map.entry(ele).or_insert(0);
-        *count += 1;
+        *map.entry(ele).or_insert(0) += 1;
     }
-    let mut sum: i32 = 0;
-    for ele in left {
-        let val = map.get(&ele);
-        let i = if val.is_none() { 0 } else { *val.unwrap() };
-        sum += ele * i;
-    }
+    let sum: i32 = left.iter().map(|ele| ele * map.get(ele).unwrap_or(&0)).sum();
     Some(sum.try_into().unwrap())
 }
 
-fn parse(input: &str) -> (Vec<i32>, Vec<i32>) {
+fn parse(input: &str) -> (Vec<i32> , Vec<i32>) {
     // Read the contents of the file
     let mut left: Vec<i32> = Vec::new();
     let mut right: Vec<i32> = Vec::new();
-
     for line in input.lines() {
         // Parse the line
-        let items: Vec<i32> = line
-            .split("   ")
-            .map(|x: &str| x.parse::<i32>().unwrap())
-            .collect();
+        let items: Vec<i32> = line.split("   ").map(|x: &str| x.parse::<i32>().unwrap()).collect();
         left.push(items[0]);
         right.push(items[1]);
-    }
 
+    }
     (left, right)
 }
 
@@ -63,4 +54,5 @@ mod tests {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(31));
     }
+
 }
